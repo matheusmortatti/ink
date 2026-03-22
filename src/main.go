c4a3272bf0380e05386646d8a54a641c68a65f1d
Matheus Mortatti <matheusmortatti@gmail.com>
2026-03-21 19:41:55 -0700

    add a test using text area

--- src/main.go
+++ src/main.go
@@ -3,33 +3,80 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"charm.land/bubbles/v2/textarea"
 	tea "charm.land/bubbletea/v2"
 )
 
-type model struct{}
+type model struct {
+	textarea textarea.Model
+	err      error
+}
+
+func initialModel() model {
+	ti := textarea.New()
+	ti.SetVirtualCursor(true)
+	ti.Focus()
+
+	return model{
+		textarea: ti,
+		err:      nil,
+	}
+}
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(textarea.Blink, tea.RequestBackgroundColor)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyPressMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "esc":
+			if m.textarea.Focused() {
+				m.textarea.Blur()
+			}
+		case "ctrl+c":
 			return m, tea.Quit
+		default:
+			if !m.textarea.Focused() {
+				cmd = m.textarea.Focus()
+				cmds = append(cmds, cmd)
+			}
 		}
 	}
-	return m, nil
+	m.textarea, cmd = m.textarea.Update(msg)
+	cmds = append(cmds, cmd)
+	return m, tea.Batch(cmds...)
 }
 
 func (m model) View() tea.View {
-	return tea.NewView("Hello, World! Press q to quit.\n")
+	const (
+		footer = "\nPress Ctrl+C to quit.\n"
+	)
+
+	var c *tea.Cursor
+
+	if !m.textarea.VirtualCursor() {
+		c = m.textarea.Cursor()
+	}
+
+	f := strings.Join([]string{
+		m.textarea.View(),
+		footer,
+	}, "\n")
+
+	v := tea.NewView(f)
+	v.Cursor = c
+	return v
 }
 
 func main() {
-	p := tea.NewProgram(model{})
+	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
